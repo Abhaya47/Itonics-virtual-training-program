c07767ded4f6e4d5450d315008c8b0b1257fbb53
dhirajs16 <sarrafd16@gmail.com>
2026-02-07 16:45:32 +0545

    KEY-01 Pushing local code for Merge Conflict

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 
     <h1>Student Course Enrollment System</h1>
     <h4>I am making changes to this to create a merge conflict</h4>
+    <h4>Looking to resolve the merge conflict.</h4>
+
     <ul>
         <li><a href="students/index.php">Manage Students</a></li>
         <li><a href="courses/index.php">Manage Courses</a></li>
